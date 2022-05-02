cd26f0d443e2270443509d0ed48e93b1154726e2
ntflores <100636782+ntflores@users.noreply.github.com>
2022-05-02 13:50:00 -0700

    Add files via upload

---
+++ pdfmergeall.py
@@ -0,0 +1,12 @@
+import os
+from PyPDF2 import PdfFileMerger
+source_dir = os.getcwd()
+
+merger = PdfFileMerger()
+
+for item in os.listdir(source_dir):
+    if item.endswith('pdf'):
+        merger.append(item)
+
+merger.write("complete.pdf")
+merger.close
